a7b1cac8b430b38f20b5cd8dfd29fd1283c05a74
lart <lartpang@163.com>
2024-04-13 00:00:09 +0800

    Refactoring based on process pools and cross-process communication managers.

--- examples/demo.py
+++ examples/demo.py
@@ -1,1 +1,13 @@
-print('Hello!')
+import argparse
+import os
+import time
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--value", type=int, default=0)
+args = parser.parse_args()
+
+
+GPU_IDS = os.environ["CUDA_VISIBLE_DEVICES"]
+print(f"[GPUs: {GPU_IDS}] Start {args.value}")
+time.sleep(args.value * 2)
+print(f"[GPUs: {GPU_IDS}] End {args.value}")
